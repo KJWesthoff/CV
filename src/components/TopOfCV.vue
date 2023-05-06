637b59aaad0e35a882b5eefddc70a295c8f312f3
KJWesthoff <karl.johan.westhoff@gmail.com>
2023-05-05 18:08:05 -0700

    got tailwind running

---
+++ src/components/TopOfCV.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+  <div class="header container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 w-200">
+    
+    <div class="columns-2">
+      
+      <div class="w-full">
+        <h1 class="text-3xl font-bold">Karl-Johan Westhoff</h1>
+        <h4>Engineer</h4>
+      </div>
+
+      <div class="w-full text-right">
+      
+      <p>Tel: (408) 416-1761</p>
+      <p>email: karl.johan.westhoff@gmail.com</p>
+    </div>
+    </div>
+    
+    <div class="about">
+      <p>10+ yeas of experience in designing and building structures for the Ocean, hereunder making the software tools
+        for doing the engineering. I have extensive experience with managing client facing and technically challenging
+        projects of various size.</p>
+    </div>
+
+  </div>
+</template>
+
+
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/CV/'
+  base:'/CV/',
+  server: {
+    port:8080,
+  }
 })
