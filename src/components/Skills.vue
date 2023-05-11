962b447b8666e2f2418a3eebc11e4f90629fccdd
KJWesthoff <karl.johan.westhoff@gmail.com>
2023-05-11 13:34:52 -0700

    workin on it

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -18,17 +18,21 @@ const viewBox = computed(() => {
 
 const radiusScale = computed(() => d3.scaleSqrt().domain([1, 5]).range([5, 15]));
 
-const simulation = computed(() => d3.forceSimulation()
+const simulation = computed(()=> d3.forceSimulation()
     .force('charge', d3.forceManyBody().strength(5))
     .force('center', d3.forceCenter(width / 2, height / 2))
     .force('ground', d3.forceY(height))
     .force('x', d3.forceX().x(width / 2))
     .force('collision', d3.forceCollide().radius(d => radiusScale.value(d.level)))
-)
+    )
 
 
 const ticked = computed(() => {
-    const svg = d3.select("svg g")
+    
+    const svg = d3.select("svg")
+    console.log("tick tack")
+    console.log(svg.selectAll(".scircle"))
+    
     svg.selectAll(".scircle")
         .attr('cx', function (d) {
             console.log(d)
@@ -37,23 +41,20 @@ const ticked = computed(() => {
         .attr('cy', function (d) {
             return d.y;
         });
-    svg.selectAll(".stext")
-        .attr('x', function (d) {
-            return d.x;
-        })
-        .attr('y', function (d) {
-            return d.y;
-        });
+    // svg.selectAll(".stext")
+    //     .attr('x', function (d) {
+    //         return d.x;
+    //     })
+    //     .attr('y', function (d) {
+    //         return d.y;
+    //     });
 })
 
 
 
 
-onMounted(() => {
-
+watchEffect(() => {
     simulation.value.nodes(data.skills.software.webstack).on("tick", ticked.value)
-
-
 })
 
 
@@ -72,19 +73,19 @@ onMounted(() => {
         </div>
         <div id="bubbles" class="h-100">
             <svg class="svg-holder" :viewBox="viewBox">
-                <g :transform="'translate(100,100)'">
+                <g>
                     <circle v-for="(d, i) in data.skills.software.webstack" :r="radiusScale(d.level)"
                         class="scircle fill-blue-300">
                     </circle>
                 </g>
-                <g :transform="'translate(100,100)'">
+                <!-- <g :transform="'translate(100,100)'">
                     <text v-for="(d, i) in data.skills.software.webstack" class="stext text-center text-s">
                     {{ d.title }}
                     </text>
-                </g>
-                <g :transform="'translate(100,100)'">
-                    <icon width = "5" height = "3"  v-for="(d, i) in data.skills.software.webstack" key="skill.title" :name=d.logo></icon>
-                </g>
+                </g> -->
+                <!-- <g :transform="'translate(100,100)'">
+                    <icon width = "5" height = "3"  v-for="(d, i) in data.skills.software.webstack" :key="d.title" :name=d.logo></icon>
+                </g> -->
             </svg>
         </div>
 
