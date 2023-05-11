def769cda88b514654f31e99dd71880776274020
KJWesthoff <karl.johan.westhoff@gmail.com>
2023-05-10 20:20:19 -0700

    addin d3 bubbles for skills

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,28 @@
 <script setup>
+import data from '../assets/static/cv_data.json'  
+console.log(data)
 </script>
 
 <template>
   <div class="header container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 w-200">
 
-    <div class="flex items-center">
+    <div class="flex justify-between align-bottom mb-5 items-end">
 
-      <div class="w-full">
-        <h1 class="text-3xl font-bold">Karl-Johan Westhoff</h1>
+      <div>
+        <h1 class="text-3xl font-bold">{{data.about.name}}</h1>
         <h4>Engineer</h4>
       </div>
 
-      <div class="w-full text-right grid grid-flow-col gap-4 p-5">
-        <p class="text-blue-300">
-          <icon name = "phone" />: (408) 416-1761
-        </p>
-        <p>: karl.johan.westhoff@gmail.com</p>
+      
+      <div class="text-right items-end">
+          <div class="flex items-center"><icon name = "phone.svg" class="items-center mr-2"></icon>{{data.about.phone}}</div>
+          <div class="flex items-center"><icon name = "email.svg" class="items-center mr-2"></icon>{{data.about.email}}</div>
       </div>
+    
     </div>
 
     <div class="about">
-      <p>10+ yeas of experience in designing and building structures for the Ocean, hereunder making the software tools
-        for doing the engineering. I have extensive experience with managing client facing and technically challenging
-        projects of various size.</p>
+      <p>{{data.about.summary}}</p>
     </div>
   </div>
 </template>
