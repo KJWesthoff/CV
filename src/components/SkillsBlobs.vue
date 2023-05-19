d513bb4fa7199df00be45b71685edcf06576dcf2
KJWesthoff <karl.johan.westhoff@gmail.com>
2023-05-19 12:59:26 -0700

    clean it up force transition now working, adding txt to correct location comes next

--- src/components/SkillsBlobs.vue
+++ src/components/SkillsBlobs.vue
@@ -15,12 +15,8 @@ const width = 300;
 const height = 200;
 
 const minBlobRadius = 5
-const maxBlobRadius = 15
-const iconSize = minBlobRadius*2;
+const maxBlobRadius = 12
 const props = defineProps(['data'])
-
-
-
 
 const viewBox = computed(() => {
     return `0 0 ${width} ${height}`
@@ -29,50 +25,59 @@ const viewBox = computed(() => {
 const radiusScale = computed(() => d3.scaleSqrt().domain([1, 5]).range([minBlobRadius, maxBlobRadius]));
 
 const simulation = computed(() => d3.forceSimulation()
-    .force('charge', d3.forceManyBody().strength(5))
+    .force('charge', d3.forceManyBody().strength(10))
     .force('center', d3.forceCenter(width / 2, height / 2))
-    .force('ground', d3.forceY(height))
-    .force('x', d3.forceX().x(width / 2))
     .force('collision', d3.forceCollide().radius(d => radiusScale.value(d.level)))
+)
+
+const simulation2 = computed(() => d3.forceSimulation()
+    .force('charge', d3.forceManyBody().strength(-1))
+    .force("x", d3.forceX().x(d => {
+       
+        return xPos(d.group)
+    }))
+    .force("y", d3.forceY(1).y(height/2))
+    //.force('collision', d3.forceCollide().radius(d => radiusScale.value(d.level)/4))
 )
 
 
 // Get the tailwind colors from config
 const twConfig = resolveConfig(tailwindConfig)
-console.log(twConfig.theme.colors.green[200])
-const baseColors = twConfig.theme.colors
 
 // remove the relative color choises from the tw list
 //const colorList = Object.keys(baseColors).filter(i => !['inherit', 'current', 'transparent', 'neutral','black','white','red','pink','rose','violet'].includes(i) ).reverse()
-const colorList = ['green', 'indigo', 'fuchsia','zinc','blue']
-console.log(colorList)
+const colorList = ['green', 'blue', 'orange', 'zinc', 'slate','rose']
 
 
+
+// bastard variable with a list of the top keys in data (for ordial scalig etc.)
 const groups = props.data.map(d => d.group)
+
+
 const twColor = d3.scaleOrdinal().domain(groups)
-  .range(colorList);
+    .range(colorList);
 
 
-const twValue = d3.scaleOrdinal().domain([1,5])
-  .range([50,100,200,300,400,500,600,700,800,900]);
+// functon to set shading value based on item.level
+const twValue = d3.scaleOrdinal().domain([1, 2, 3, 4, 5])
+    .range([50, 100, 200, 300, 400, 500, 600, 700, 800, 900]);
 
 
-
-
+const xPos = d3.scaleOrdinal().domain(groups).range([...Array(groups.length).keys()].map(i => (i + 1) * width / (groups.length + 1)))
 
 // Process the data into a flat structure
-const data = props.data.reduce((a,c) => {
+const data = props.data.reduce((a, c) => {
     const newItems = [...c.items]
     newItems.map(i => {
         i.name = c.name
         i.group = c.group
     })
-    return a.concat(newItems)              
+    return a.concat(newItems)
 }, [])
 
 
 onMounted(() => {
-  
+
     const icons = d3.selectAll(".icons")
         .data(data);
     const circles = d3.selectAll(".scircle")
@@ -84,10 +89,10 @@ onMounted(() => {
     function ticked() {
         icons
             .attr('x', function (d) {
-                return d.x- radiusScale.value(d.level)/2;
+                return d.x - radiusScale.value(d.level) / 2;
             })
             .attr('y', function (d) {
-                return d.y- radiusScale.value(d.level)/2;
+                return d.y - radiusScale.value(d.level) / 2;
             })
             .attr('height', function (d) {
                 return radiusScale.value(d.level);
@@ -95,7 +100,7 @@ onMounted(() => {
             .attr('width', function (d) {
                 return radiusScale.value(d.level);
             })
-      
+
         circles
             .attr('cx', function (d) {
                 return d.x;
@@ -106,29 +111,73 @@ onMounted(() => {
             .attr('fill', function (d) {
                 const cName = twColor(d.group)
                 const cValue = twValue(d.level)
-                return  twConfig.theme.colors[cName][cValue];
+                return twConfig.theme.colors[cName][cValue];
             })
     }
 })
+
+const tidyUpClick = () => {
+    const icons = d3.selectAll(".icons")
+        .data(data);
+    const circles = d3.selectAll(".scircle")
+        .data(data);
+    const texts = d3.selectAll(".group-text")
+        .data(props.data)
+        .text(d => d.name)
+        .attr("x", d=>xPos(d.group))
+        .attr("y", height/4)
+        .style("text-anchor", "middle")
+        .attr("font-size", 5)        
+    
+
+    
+    simulation2.value.nodes(data).on("tick", ticked)
+
+    function ticked() {
+        icons
+            .attr('x', function (d) {
+                return d.x - radiusScale.value(d.level) / 2;
+            })
+            .attr('y', function (d) {
+                return d.y - radiusScale.value(d.level) / 2;
+            })
+
+
+        circles
+            .attr('cx', function (d) {
+                return d.x;
+            })
+            .attr('cy', function (d) {
+                return d.y;
+            })
+
+    }
+
+}
+
 </script>
 
 <template>
-         <div id="bubbles" class="h-100">
-            <svg class="svg-holder" :viewBox="viewBox">
-               <g v-for="(d, i) in data" :key="d.title">
-                        <circle :name="d.title" :r="radiusScale(d.level)" class="scircle stroke-black stroke-1">
-
-                        </circle>
-                        
-                        <svg class = "icons">
-                            <icon :name=d.logo></icon>
-                        </svg>
-                    
+    <button :onClick=" tidyUpClick ">Clean it up Johnny</button>
+    <div id="bubbles" class="h-100">
+        <svg class="svg-holder" :viewBox="viewBox">
+           <g v-for="g in props.data" :key = "g">
+            <text class="group-text text-center" :id ="g">
                 
+            </text>    
+        </g>     
+
+            <g v-for="(d, i) in data" :key="d.title">
+                <circle :name="d.title" :r="radiusScale(d.level)" class="scircle stroke-black stroke-1">
+
+                </circle>
+
+                <svg class="icons">
+                    <icon :name=d.logo></icon>
+                </svg>
+
+
             </g>
-            </svg>
-        </div>
-
-
-
+        </svg>
+    </div>
 </template>
