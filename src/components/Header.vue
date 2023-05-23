ff803388bba44bff4b246e28286794087156b134
KJWesthoff <karl.johan.westhoff@gmail.com>
2023-05-23 13:15:11 -0700

    flex wrapped the name in the header so it look better on mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,34 @@
 <script setup>
-import data from '../assets/static/cv_data.json'  
+import data from '../assets/static/cv_data.json'
 
 </script>
 
 <template>
   <div class="header container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 w-200">
 
-    <div class="flex justify-between align-bottom mb-5 items-end">
+    <div class="flex justify-between align-bottom mb-5 items-end flex-wrap">
 
       <div>
-        <h1 class="text-3xl font-bold">{{data.about.name}}</h1>
+        <h1 class="text-3xl font-bold">{{ data.about.name }}</h1>
         <h4 class="text-lg font-bold underline underline-offset-4">Engineer</h4>
       </div>
 
-      
+
       <div class="text-right items-end">
-          <div class="flex items-center"><icon name = "phone.svg" class="items-center mr-2"></icon>{{data.about.phone}}</div>
-          <div class="flex items-center"><icon name = "email.svg" class="items-center mr-2"></icon>{{data.about.email}}</div>
+        <div class="flex items-center">
+          <icon name="phone.svg" class="items-center mr-2"></icon>{{ data.about.phone }}
+        </div>
+        <div class="flex items-center">
+          <icon name="email.svg" class="items-center mr-2"></icon>{{ data.about.email }}
+        </div>
       </div>
-    
+
     </div>
 
     <div class="about">
-      <p>{{data.about.summary}}</p>
+      <p>{{ data.about.summary }}</p>
     </div>
-    
+
   </div>
 </template>
 
