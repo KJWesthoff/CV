524a34c6b02338cd97f4a3e847c2456f1f9e184a
KJWesthoff <KJWesthoff@users.noreply.github.com>
2023-05-23 20:57:49 +0000

    deploy: 71793486cc4b54de76c5f69f2652e3616037546d

--- assets/flexible-947ae9a2.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as o,a as t}from"./index-19d3da5a.js";const s={xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 15 15"},a=t("path",{stroke:"#000",d:"M2.5 1.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm0 0h2a3 3 0 0 1 3 3v6a3 3 0 0 0 3 3h2m0 0a1 1 0 1 0 2 0 1 1 0 0 0-2 0Z"},null,-1),n=[a];function c(r,l){return e(),o("svg",s,n)}const h={render:c};export{h as default,c as render};
