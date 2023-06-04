cf2ab29e28af01fc735b04cd8a701bcf7c24db36
KJWesthoff <KJWesthoff@users.noreply.github.com>
2023-06-04 16:26:35 +0000

    deploy: 401b82e382cd7a37791b2606f565003b0d0a4e29

---
+++ assets/downarrow-8565f9f2.js
@@ -0,0 +1,1 @@
+import{o as e,c as o,a as t}from"./index-4c2d0749.js";const s={xmlns:"http://www.w3.org/2000/svg","xml:space":"preserve",viewBox:"0 0 512 512"},r=t("path",{d:"M289.7 341.3V0h-85.4v341.3L33.7 170.7v128L247 512l213.3-213.3v-128z"},null,-1),c=[r];function n(a,d){return e(),o("svg",s,c)}const _={render:n};export{_ as default,n as render};
