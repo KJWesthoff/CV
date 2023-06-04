401b82e382cd7a37791b2606f565003b0d0a4e29
KJWesthoff <karl.johan.westhoff@gmail.com>
2023-06-04 09:25:40 -0700

    added links to print version

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -18,15 +18,17 @@ function pageChange(s) {
 
 <template>
     <div class="header container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 w-200">
-
+        
         <div>
             <div v-if="page == 0">
                 <h1 class="text-lg font-bold underline underline-offset-4">Skills</h1>
                 <span class = "italic print:hidden">(Hover for details Click to clean it up)</span>
+             
             </div>
             <div v-if="page == 1">
                 <h1 class="text-lg font-bold underline underline-offset-4">Skills Categorized</h1>
                 <span class = "italic print:hidden">(Click again to show the connections)</span>
+                
             </div>
             <div v-if="page == 2">
             <div class="flex justify-around">
@@ -35,10 +37,12 @@ function pageChange(s) {
                 <h1 class="text-lg font-bold underline underline-offset-4">Experience</h1>
             </div>  
             <span class = "italic print:hidden">(Click to Re-set)</span>
+           
             </div>
 
         </div>
         <SkillsBlobs @page-change="(n) => pageChange(n)" :data="data"></SkillsBlobs>
+        <span class = "italic hidden print:block"><a href="https://kjwesthoff.github.io/CV/">(kjwesthoff.github.io/CV/)</a></span>
 
 
     </div>
